054be647a2a6054f298fd821e5f9c2c705bf6083
Осипов Илья Иванович <st807785@ad.pu.ru>
2024-03-08 20:42:52 +0300

    создает проект и базовую структуру

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from "@angular/common";
+import {SelectorComponent} from "./components/selector/selector.component";
+import {CalendarComponent} from "./components/calendar/calendar.component";
+import {OrganizerComponent} from "./components/organizer/organizer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SelectorComponent, CalendarComponent, OrganizerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
